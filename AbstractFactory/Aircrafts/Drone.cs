8ab37cf5b9c436ad74d5055f60546f5f0e927423
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2023-07-25 13:33:31 -0300

    AbstractFactory

---
+++ AbstractFactory/Aircrafts/Drone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AbstractFactory.Aircrafts
+{
+    class Drone : IAircraft
+    {
+        public void CheckWind()
+        {
+            Console.WriteLine("Verificando ventos, ventos a 5km, ventos ok!");
+        }
+
+        public void GetCargo()
+        {
+            Console.WriteLine("Estamos com a encomanda!");
+        }
+
+        public void StartRoute()
+        {
+            CheckWind();
+            GetCargo();
+            Console.WriteLine("Estamos a caminho do destino!");
+        }
+    }
+}
---
+++ AbstractFactory/LandVehicles/Scooter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbstractFactory.LandVehicles
+{
+    class Scooter : ILandVehicle
+    {
+        public void GetCargo()
+        {
+            Console.WriteLine("Pegamos a encomenda, estamos pronto para partir!");
+        }
+
+        public void StartRoute()
+        {
+            GetCargo();
+            Console.WriteLine("Iniciando a entrega!");
+        }
+    }
+}
